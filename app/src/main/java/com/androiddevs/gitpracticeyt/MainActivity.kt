4be68713a9f44f93f67af58b0317b1bb8406c812
Moslem Hoveizavi <mslmhv@gmail.com>
2023-06-22 22:02:50 +0330

    Third commit

--- app/src/main/java/com/androiddevs/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/androiddevs/gitpracticeyt/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             GitPracticeYTTheme {
                 println("Second commit")
+                hello error
             }
         }
     }
